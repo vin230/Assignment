822aa71100fd2cb9c3b917d0eeb5293bbc96d67d
vin230 <vinoddesai810@gmail.com>
2021-08-14 23:13:16 +0530

    initial commit

---
+++ src/screens/TabletScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, View , Image, FlatList} from 'react-native';
+
+const DATA=[
+  {
+    image:require('../assets/Image4.jpg'),
+    key:4
+  },
+
+]
+const HelloWorldApp = () => {
+  const renderItem = ({ item }) => (
+    <View style={{margin:5}}>
+    <Image style={{height:300, width:180}} source={item.image}/>
+    </View>
+  );
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        //alignItems: "center"
+      }}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.key}
+        numColumns={2}
+      />
+    </View>
+  )
+}
+export default HelloWorldApp;
